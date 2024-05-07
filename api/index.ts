617cbfed1db39c12322bc78a45c314580e60b64c
paresiqbal <thecrowdel@gmail.com>
2024-05-07 20:30:16 +0700

    asdasd

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-app.use("/user", UserRouter);
+app.get("/user", UserRouter);
 
 mongoose.connect(`${process.env.MONGO_URI}`);
 
