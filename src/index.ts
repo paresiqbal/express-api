b8978d837af9c15a637e90a6b18e03e8aac0b9d4
paresiqbal <thecrowdel@gmail.com>
2024-05-17 15:10:34 +0700

    Remove Vercel configuration and project files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { UserModel } from "../src/models/UserModel";
+import { UserModel } from "./models/UserModel";
 
 dotenv.config();
 
